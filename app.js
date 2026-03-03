45c2feb9a754b492e4f9d77aa1eb70c371c223a6
Jacky Jiang <jj3328@drexel.edu>
2026-03-03 15:18:55 -0500

    Changed the check for logged to be just a redirect because the script was breaking posts.

--- app.js
+++ app.js
@@ -52,12 +52,7 @@ const requireGuest = (req, res, next) => {
 //Middleware for checking if the user is logged in so that they can make a post
 const requireLogin = (req, res, next) => {
   if (!req.session.userId) {
-    return res.send(`
-      <script>
-        alert("You must be logged in to continue.");
-        window.location.href = "/login";
-      </script>
-    `);
+    return res.redirect('/login');
   }
   next();
 };
